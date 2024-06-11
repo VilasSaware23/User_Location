60c1942433002af775c96fc0ee5c5926f08af370
VilasSaware23 <vilassaware63@gmail.com>
2024-06-11 14:19:36 +0530

    first commit

---
+++ My_Current_Location/My_Current_Location/ViewController.swift
@@ -0,0 +1,86 @@
+//
+//  ViewController.swift
+//  My_Current_Location
+//
+//  Created by MSC_INDIA on 07/06/24.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var myMap: MKMapView!
+    
+    let locationManager = CLLocationManager()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        if(CLLocationManager.locationServicesEnabled())
+        {
+            locationManager.requestLocation()
+            locationManager.startUpdatingLocation()
+        }
+    }
+
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let userLocation = locations.first
+        {
+            manager.stopUpdatingLocation()
+            
+            let coordinates = CLLocationCoordinate2D(latitude: locationManager.location?.coordinate.latitude ?? 0.0, longitude: locationManager.location?.coordinate.longitude ?? 0.0)
+            
+            let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+            
+            let region = MKCoordinateRegion(center: coordinates, span: span)
+            
+            myMap.setRegion(region, animated: true)
+            
+            let myPin = MKPointAnnotation()
+            myPin.coordinate = coordinates
+            
+            myPin.title = "CABCS GROUP INDIA"
+            myPin.subtitle = "Bengaluru"
+            
+            myMap.addAnnotation(myPin)
+        }
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+     switch manager.authorizationStatus {
+         case .authorizedAlways:
+              return
+            
+         case .authorizedWhenInUse:
+              return
+            
+         case .denied:
+              return
+            
+         case .restricted:
+          locationManager.requestWhenInUseAuthorization()
+            
+         case .notDetermined:
+          locationManager.requestWhenInUseAuthorization()
+         
+         default:
+          locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+    
+    
+    
+    
+    
+}
+
